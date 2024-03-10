8c72292b1c63b077394949426fc495b97a042cf0
Juuso <ejuhe003@edu.xamk.fi>
2024-03-10 14:03:26 +0200

    Saatu tietoa haettua tietokannasta ja tehty myös funktio jolla voi lisätä tietokantaan uusi tehtävä

--- ReactWithASP.Server/Controllers/TaskController.cs
+++ ReactWithASP.Server/Controllers/TaskController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Data;
+using System.Net.NetworkInformation;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ReactWithASP.Server.Controllers
 {
@@ -19,6 +22,7 @@ namespace ReactWithASP.Server.Controllers
         [HttpGet]
         [Route("GetTasks")]
 
+        //Funktio jolla saadaan tietokannasta tehtävän tiedot
         public JsonResult GetTasks()
         {
             string query = "select * from dbo.Task";
@@ -38,5 +42,150 @@ namespace ReactWithASP.Server.Controllers
             }
             return new JsonResult(table);
         }
+
+        [HttpGet]
+        [Route("GetTags")]
+
+        //Funktio jolla saadaan tagin tiedot
+        public JsonResult GetTags() 
+        {
+            string query = "select * from dbo.Tag";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand MyCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = MyCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+
+        }
+
+        [HttpGet]
+        [Route("GetStatus")]
+
+        //Funktio jolla saadaan status
+        public JsonResult GetStatus()
+        {
+            string query = "select * from dbo.Status";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand MyCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = MyCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+
+
+        }
+
+        [HttpGet]
+        [Route("AddTask")]
+
+        public JsonResult AddTask(string Name, string Description, DateTime StartDate, DateTime EndDate, string TagName, string TagTheme, string StatusName, string StatusTheme)
+        {
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                int TagId = InsertTag(TagName, TagTheme);
+
+                int statusId = InsertStatus(StatusName, StatusTheme);
+
+                string insertQuery = "INSERT INTO Task (Name, Description, StartDate, EndDate, TagId, StatusId) " +
+                                "VALUES (@Name, @Description, @StartDate, @EndDate, @TagId, @StatusId)";
+
+
+                using (SqlCommand MyCommand = new SqlCommand(insertQuery, myCon))
+                {
+                    MyCommand.Parameters.AddWithValue("@Name", Name);
+                    MyCommand.Parameters.AddWithValue("@Description", Description);
+                    MyCommand.Parameters.AddWithValue("@StartDate", StartDate);
+                    MyCommand.Parameters.AddWithValue("@EndDate", EndDate);
+                    MyCommand.Parameters.AddWithValue("@TagId", TagId);
+                    MyCommand.Parameters.AddWithValue("@StatusId", statusId);
+                    myReader = MyCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult("Lisäys onnistui");
+
+
+        }
+        private int InsertTag(string TagName, string TagTheme)
+        {
+          
+            string insertTagQuery = "IF NOT EXISTS (SELECT 1 FROM Tag WHERE Name = @TagName) " +
+                                    "BEGIN " +
+                                    "    INSERT INTO Tag (Name, Theme) VALUES (@TagName, @TagTheme) " +
+                                    "END " +
+                                    "SELECT Id FROM Tag WHERE Name = @TagName";
+            
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(insertTagQuery, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@TagName", TagName);
+                    myCommand.Parameters.AddWithValue("@TagTheme", TagTheme);
+                    myCommand.ExecuteNonQuery();
+                    int TagId = (int)myCommand.ExecuteScalar();
+                    myCon.Close();
+                    return TagId;
+                }
+            }
+            
+        }
+
+        private int InsertStatus(string StatusName, string StatusTheme)
+        {
+            string insertStatusQuery = "IF NOT EXISTS (SELECT 1 FROM Status WHERE Name = @StatusName) " +
+                                       "BEGIN " +
+                                       "    INSERT INTO Status (Name, Theme) VALUES (@StatusName, @StatusTheme) " +
+                                       "END " +
+                                       "SELECT Id FROM Status WHERE Name = @StatusName";
+          
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand myCommand = new SqlCommand(insertStatusQuery, myCon))
+                {
+                    myCommand.Parameters.AddWithValue("@StatusName", StatusName);
+                    myCommand.Parameters.AddWithValue("@StatusTheme", StatusTheme);
+
+                    myCommand.ExecuteNonQuery();
+                    int statusId = (int)myCommand.ExecuteScalar();
+                    myCon.Close();
+                    return statusId;
+                }
+            }
+            
+        }
+
+
     }
 }
