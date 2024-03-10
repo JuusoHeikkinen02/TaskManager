cdfceaeb9226cacd6b98f1363dc1f33d82e66c34
Juuso <ejuhe003@edu.xamk.fi>
2024-03-10 15:24:39 +0200

    Saatu funktio jolla voi poistaa tehtävän ja tähän relaatiossa olevat tagit ja statuksen

--- ReactWithASP.Server/Controllers/TaskController.cs
+++ ReactWithASP.Server/Controllers/TaskController.cs
@@ -3,6 +3,7 @@ using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore.Metadata.Internal;
 using System.Data;
 using System.Net.NetworkInformation;
+using System.Threading.Tasks;
 using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
 
 namespace ReactWithASP.Server.Controllers
@@ -96,6 +97,29 @@ namespace ReactWithASP.Server.Controllers
         }
 
         [HttpGet]
+        [Route("GetActivity")]
+        //Funktio jolla saadaan aktiviteetin tiedot 
+        public JsonResult GetActivity()
+        {
+            string query = "select * from dbo.Activity";
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            SqlDataReader myReader;
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand MyCommand = new SqlCommand(query, myCon))
+                {
+                    myReader = MyCommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    myCon.Close();
+                }
+            }
+            return new JsonResult(table);
+        }
+
+        [HttpPut]
         [Route("AddTask")]
 
         public JsonResult AddTask(string Name, string Description, DateTime StartDate, DateTime EndDate, string TagName, string TagTheme, string StatusName, string StatusTheme)
@@ -132,6 +156,8 @@ namespace ReactWithASP.Server.Controllers
 
 
         }
+
+        //täällä saadaan Tägi luotua samalla kun tehdään uusi Task tai Activity
         private int InsertTag(string TagName, string TagTheme)
         {
           
@@ -159,6 +185,7 @@ namespace ReactWithASP.Server.Controllers
             
         }
 
+        //täällä saadaan status luotua samalla kun tehdään uusi Task tai Activity
         private int InsertStatus(string StatusName, string StatusTheme)
         {
             string insertStatusQuery = "IF NOT EXISTS (SELECT 1 FROM Status WHERE Name = @StatusName) " +
@@ -184,6 +211,99 @@ namespace ReactWithASP.Server.Controllers
                 }
             }
             
+        }
+
+        //Funktio jolla voidaan poistaa tehtävä ja sen pitäisi samalla poistaa status ja tagi 
+        [HttpDelete]
+        [Route("DeleteTask")]
+
+        public JsonResult DeleteTask(int Id)
+        {
+            DataTable table = new DataTable();
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+
+                //Hankitaan tehtävän avulla näille arvot jotta ne voidaan myös poistaa
+                int TagId;
+                int StatusId;
+
+                string getIdsQuery = "SELECT TagId, StatusId FROM Task WHERE Id = @Id";
+                using (SqlCommand MyCommand = new SqlCommand(getIdsQuery, myCon))
+                {
+                    MyCommand.Parameters.AddWithValue("@Id", Id);
+                    
+                    using (SqlDataReader reader = MyCommand.ExecuteReader())
+                    {
+                        if (reader.Read())
+                        {
+                            TagId = reader.GetInt32(0);
+                            StatusId = reader.GetInt32(1);
+                        }
+                        else
+                        {
+                            // Task not found
+                            return new JsonResult("Tehtävää ei löytynyt");
+                        }
+                    }
+                }
+                // Tehtävän poisto
+                string deleteTaskQuery = "DELETE FROM Task WHERE Id = @Id";
+                using (SqlCommand MyCommand = new SqlCommand(deleteTaskQuery, myCon))
+                {
+                    MyCommand.Parameters.AddWithValue("@Id", Id);
+                    MyCommand.ExecuteNonQuery();
+                    
+                }
+                //Lopuksi poistetaan tag ja status joka oli yhdessä tehtävän kanssa
+                DeleteTag(TagId);
+                DeleteStatus(StatusId);
+
+                myCon.Close();
+
+            }
+            return new JsonResult("Poistettu onnistuneesti");
+        }
+
+        //Komento jolla poistetaan tägi samalla kun tehtävä poistuu
+        private void DeleteTag(int Id)
+        {
+            string deleteTagQuery = "DELETE FROM dbo.Tag WHERE Id = @Id";
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand deleteCommand = new SqlCommand(deleteTagQuery, myCon))
+                {
+
+                    deleteCommand.Parameters.AddWithValue("@Id", Id);
+                    deleteCommand.ExecuteNonQuery();
+                    myCon.Close();
+                    
+                }
+            }
+            
+        }
+        //Funktio jolla saadaan poistettua status kun tehtävä tai aktiviteetti poistetaan
+        private void DeleteStatus(int Id)
+        {
+            string deleteStatusQuery = "DELETE FROM dbo.Status WHERE Id = @Id";
+            string sqlDatasource = _configuration.GetConnectionString("taskDBCon");
+            using (SqlConnection myCon = new SqlConnection(sqlDatasource))
+            {
+                myCon.Open();
+                using (SqlCommand deleteCommand = new SqlCommand(deleteStatusQuery, myCon))
+                {
+
+                    deleteCommand.Parameters.AddWithValue("@Id", Id);
+                    deleteCommand.ExecuteNonQuery();
+                    myCon.Close();
+
+                }
+            }
+
         }
 
 
